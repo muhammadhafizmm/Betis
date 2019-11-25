b7533468a9458d65e9d3a14f3edae67180c3f284
muhammadhafizm <m.hafizmaulana777@gmail.com>
2019-11-25 23:17:16 +0700

    third, comment

--- template.js
+++ template.js
@@ -1,9 +1,11 @@
+// js for materi and polygon
 var x = document.getElementsByClassName("ilmu");
 var y = document.getElementsByClassName("polygon")
 function openIlmu(n){
     x[n-1].classList.toggle("tampil");
     y[n-1].classList.toggle("muter");
 }
+// js for navbar dropDown
 function openMenu() {
     document.getElementById("dropDown").classList.toggle("show");
     }
